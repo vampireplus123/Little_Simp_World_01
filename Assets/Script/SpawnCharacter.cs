255724c18e387e9676d29e243796a9d95ef04f1d
vampireplus123 <66988477+vampireplus123@users.noreply.github.com>
2022-08-13 19:08:56 +0700

    Anh huynh

---
+++ Assets/Script/SpawnCharacter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnCharacter : MonoBehaviour
+{
+    public GameObject obj;
+    Vector3 spawnpoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        Instantiate(obj,transform.position,transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
